a6a1d0fd69ff92e78e19b52b4f07e6d6dd1983dd
iichanize <iichanize.irl@gmail.com>
2022-10-03 12:31:56 +0900

    outputDirをdocsに

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: "docs",
   publicPath: "./",
   devServer: {
     port: process.env.PORT,
