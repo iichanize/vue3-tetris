0ed9a8712dfd388824d96290474f9524eab34216
iichanize <iichanize.irl@gmail.com>
2022-10-07 09:47:15 +0900

    proxy設定

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
     allowedHosts: ["vue3-tetris.herokuapp.com", "localhost"],
     proxy: {
       "/ws": {
-        target: "https://vue3-tetris.herokuapp.com:" + herokuPORT,
+        target: "https://vue3-tetris.herokuapp.com",
         wss: true,
         changeOrigin: true,
       },
